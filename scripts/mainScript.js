b537ca05a067d221465f7c6c21a1246312da7d3e
Christopher Gonzalez <chriswgonz@fullsail.edu>
2013-11-13 00:33:40 -0500

    JS- Image player has more features / unified fade functions
    Image player now has image indicators and mouseover/mouseout features. Fade functions had been unified to an universal fade functionns. Some minor improvements has been added to the HTML and CSS files too...

--- scripts/mainScript.js
+++ scripts/mainScript.js
@@ -13,20 +13,22 @@
 //alert("Note: Please be aware that this Website is still currently underdevelopment... Thank you");
    
 
-// DOM Shortcuts Object ===================================================
+
+// DOM Shortcuts Object ======================================================
 var short = {
 	domId     : function (x) { var idE  = document.getElementById(x); return idE; },
 	domClass  : function (x) { var cEle = document.getElementsByClassName(x); return cEle; },
 	domTag    : function (x) { var tagE = document.getElementsByTagName(x); return tagE; },        
 	domCreate : function (x) { var crtE = document.createElement(x); return crtE; }
 };
-// End of DOM Shortcuts ===================================================
+// End of DOM Shortcuts ======================================================
+
 
 
 /* HTML Document Ready JS Function Triger ********************************* */
 window.addEventListener("DOMContentLoaded", function() {
 
-		/* Due to CSS issues, 
+		/* Due to CSS3 conflicts, 
 		JS was used for nav links changing color on hover */
 		
 		/* Nav About Link ************************************************* */ 
@@ -60,37 +62,133 @@ window.addEventListener("DOMContentLoaded", function() {
 			short.domClass("teamAnimate2")[0].style.opacity    = 0;
 		};
 		// End of Team Animation /////////////////////////////////////////////
+		
+		/* Auto Hide/Show Image Play ************************************** */
+		short.domClass("demoImgContainer")[0].onmouseover = function(){
+			short.domClass("imgFooter")[0].style.transition = "opacity 0.5s linear 0s";
+			short.domClass("imgFooter")[0].style.opacity    = 1;
+		};
+		short.domClass("demoImgContainer")[0].onmouseout = function(){
+			short.domClass("imgFooter")[0].style.transition = "opacity 0.5s linear 0s";
+			short.domClass("imgFooter")[0].style.opacity    = 0;
+		};
+		/* End Of Auto Hide/Show Image Play ******************************* */
 });	
 // End of HTML Document Ready JS Function Triger /////////////////////////////	
 	
 
-/* Contact Hidden Information Features ************************************* */
+
+/* Global Variables For Animation Features */
 var fadeCounter = 0;
 var fadeInFrom  = 0;
-var fadeOutFrom = 10;	
+var fadeOutFrom = 10;
+
+var slideCounter 	 = 0;
+var slideFadeInFrom  = 0;
+var slideFadeOutFrom = 10;		
 	
-//Show/Hide Trigger Function
-var fade              								 = function(){
+
+
+/* Contact Hidden Information Features ************************************* */
+
+// Show/Hide Trigger Function
+var fade              								 = function(fadeDivID, slideDivID){
 	if (fadeCounter	=== 0) {
 		short.domId("showContact").style.padding     = "3px 3px 5px 3px";
 		short.domId("showContact").style.borderLeft  = "2px solid #b22316";
 		short.domId("showContact").style.borderTop   = "2px solid #b22316";
 		short.domId("showContact").style.borderRight = "2px solid #b22316";
-		fadeIn("hidden123");
-		slideDown("showDiv1");
+		fadeIn(fadeDivID);
+		slideDown(slideDivID);
 		fadeCounter             					 = 1;
 	}else if (fadeCounter === 1){
 		short.domId("showContact").style.padding 	 = "5px";
 		short.domId("showContact").style.border 	 = "none";
-		fadeOut("hidden123");
-		slideUp("showDiv1");
+		fadeOut(fadeDivID);
+		slideUp(slideDivID);
 		fadeCounter             					 = 0;
 	};
 };
+// End of Contact Hidden Information Features /////////////////////////////////
+
+
+
+/* Slideshow Gallery Player ************************************************ */
+
+//Previous Image Trigger Function
+var previousArrow 									 = function(controler0, controler1, controler2, controler3){
+	if (slideCounter === 3) {
+		short.domClass("demoImg0")[0].style.display  = "none";
+		short.domTag("li")[7].style.borderBottom 	 = "5px solid transparent";
+		short.domTag("li")[6].style.borderBottom 	 = "5px solid rgba(0,0,0,0.50)";
+		fadeOut(controler3);
+		fadeIn(controler2);
+		slideCounter--;
+	}else if (slideCounter === 2){
+		short.domClass("demoImg0")[0].style.display  = "none";
+		short.domTag("li")[6].style.borderBottom 	 = "5px solid transparent";
+		short.domTag("li")[5].style.borderBottom 	 = "5px solid rgba(0,0,0,0.50)";
+		fadeOut(controler2);
+		fadeIn(controler1);
+		slideCounter--;
+	}else if (slideCounter === 1){
+		short.domTag("li")[5].style.borderBottom 	 = "5px solid transparent";
+		short.domTag("li")[4].style.borderBottom 	 = "5px solid rgba(0,0,0,0.50)";
+		fadeOut(controler1);
+		fadeIn(controler0);
+		slideCounter--;
+	}else if (slideCounter === 0){
+	    short.domTag("li")[4].style.borderBottom 	 = "5px solid transparent";
+		short.domTag("li")[5].style.borderBottom 	 = "5px solid transparent";
+		short.domTag("li")[6].style.borderBottom 	 = "5px solid transparent";
+		short.domTag("li")[7].style.borderBottom 	 = "5px solid rgba(0,0,0,0.50)";
+		fadeOut(controler0);
+		fadeIn(controler3);
+		slideCounter 								 = 3;
+	};
+};	
+
+//Next Image Trigger Function
+var nextArrow 										 = function(controler0, controler1, controler2, controler3){
+	if (slideCounter === 0) {
+		fadeOut(controler0);
+		fadeIn(controler1);
+		short.domTag("li")[4].style.borderBottom 	 = "5px solid transparent";
+		short.domTag("li")[6].style.borderBottom 	 = "5px solid transparent";
+		short.domTag("li")[7].style.borderBottom 	 = "5px solid transparent";
+		short.domTag("li")[5].style.borderBottom 	 = "5px solid rgba(0,0,0,0.50)";
+		slideCounter++;
+	}else if (slideCounter === 1){
+		short.domClass("demoImg0")[0].style.display  = "none";
+		short.domTag("li")[5].style.borderBottom 	 = "5px solid transparent";
+		short.domTag("li")[6].style.borderBottom 	 = "5px solid rgba(0,0,0,0.50)";
+		fadeOut(controler1);
+		fadeIn(controler2);
+		slideCounter++;
+	}else if (slideCounter === 2){
+		short.domClass("demoImg0")[0].style.display  = "none";
+		short.domTag("li")[6].style.borderBottom 	 = "5px solid transparent";
+		short.domTag("li")[7].style.borderBottom 	 = "5px solid rgba(0,0,0,0.50)";
+		fadeOut(controler2);
+		fadeIn(controler3);
+		slideCounter++;
+	}else if (slideCounter === 3){
+		short.domTag("li")[7].style.borderBottom 	 = "5px solid transparent";
+		short.domTag("li")[4].style.borderBottom 	 = "5px solid rgba(0,0,0,0.50)";
+		fadeOut(controler3);
+		fadeIn(controler0);
+		slideCounter 								 = 0;
+	};
+};
+// End of Slideshow Gallery Player ////////////////////////////////////////////////
 	
+
+
+/* Animation Functions  ******************************************************** */
+
 //Fade In Feature Function
-var fadeIn                   		= function(element){
-	var targetElement           	= short.domId(element);
+var fadeIn                   		= function(domTarget){
+	var targetElement           	= short.domId(domTarget);
 	targetElement.style.display 	= "block";
 	var newFadeValue            	= fadeInFrom / 10;
 	targetElement.style.opacity 	= newFadeValue;
@@ -103,26 +201,12 @@ var fadeIn                   		= function(element){
 		return false;
 			
 	};
-	var loopTimer1 					= setTimeout('fadeIn(\''+element+'\')',55);
-};	
-
-//Slide Down Function
-var slideDown 					= function(element){
-	var target 					= short.domId(element);
-	var hCheck 					= target.offsetHeight;
-	var shhCheck 				= target.scrollHeight;
-	var loopTimer 				= setTimeout('slideDown(\''+element+'\')',5);
-	if(hCheck < shhCheck){
-		hCheck += 10;
-	} else {
-		clearTimeout(loopTimer);
-	}
-	target.style.height 		= hCheck+"px";
+	var loopTimer1 					= setTimeout('fadeIn(\''+domTarget+'\')', "45");
 };
 
 //Fade Out Feature Function
-var fadeOut 						= function(element){
-	var targetElement 				= short.domId(element);
+var fadeOut 						= function(domTarget){
+	var targetElement 				= short.domId(domTarget);
 	var newFadeValue 				= fadeOutFrom / 10;
 	targetElement.style.opacity 	= newFadeValue;
 	fadeOutFrom--;
@@ -134,118 +218,36 @@ var fadeOut 						= function(element){
 		fadeOutFrom 				= 10;
 		return false;
 	};
-	var loopTimer2 					= setTimeout('fadeOut(\''+element+'\')',55);
+	var loopTimer2 					= setTimeout('fadeOut(\''+domTarget+'\')', "45");
+};
+
+//Slide Down Function
+var slideDown 					= function(domTarget){
+	var target 					= short.domId(domTarget);
+	var hCheck 					= target.offsetHeight;
+	var shhCheck 				= target.scrollHeight;
+	var loopTimer3 				= setTimeout('slideDown(\''+domTarget+'\')', "5");
+	if(hCheck < shhCheck){
+		hCheck += 10;
+	} else {
+		clearTimeout(loopTimer3);
+	}
+	target.style.height 		= hCheck+"px";
 };
 
 //Slide Up Feature Function
-var slideUp 					= function(element){
-	var target 					= short.domId(element);
+var slideUp 					= function(domTarget){
+	var target 					= short.domId(domTarget);
 	var hCheck 					= target.offsetHeight;
-	var loopTimer 				= setTimeout('slideUp(\''+element+'\')',5);
+	var loopTimer4 				= setTimeout('slideUp(\''+domTarget+'\')', "5");
 	if(hCheck > 0){
 		hCheck -= 10;
 	} else {
-		clearTimeout(loopTimer);
+		clearTimeout(loopTimer4);
 	};
 	target.style.height 		= hCheck+"px";
 };
-// End of Contact Hidden Information Features /////////////////////////////////
+/* End Of Animation Functions  ************************************************* */
 
 
-/* Slideshow Gallery Player *********************************************** */
-var slideCounter 	 = 0;
-var slideFadeInFrom  = 0;
-var slideFadeOutFrom = 10;	
-
-//Previous Image Trigger Function
-var previousArrow 									= function(){
-	if (slideCounter === 3) {
-		short.domClass("demoImg0")[0].style.display = "none";
-		slideFadeOutFunc("demoImg3");
-		slideFadeInFunc("demoImg2");
-		slideCounter--;
-	}else if (slideCounter === 2){
-		short.domClass("demoImg0")[0].style.display = "none";
-		slideFadeOutFunc("demoImg2");
-		slideFadeInFunc("demoImg1");
-		slideCounter--;
-	}else if (slideCounter === 1){
-		slideFadeOutFunc("demoImg1");
-		slideFadeInFunc("demoImg0");
-		slideCounter--;
-	}else if (slideCounter === 0){
-		slideFadeOutFunc("demoImg0");
-		slideFadeInFunc("demoImg3");
-		slideCounter 								= 3;
-	};
-};	
-
-//Next Image Trigger Function
-var nextArrow 										= function(){
-	if (slideCounter === 0) {
-		slideFadeOutFunc("demoImg0");
-		slideFadeInFunc("demoImg1");
-		slideCounter++;
-	}else if (slideCounter === 1){
-		short.domClass("demoImg0")[0].style.display = "none";
-		slideFadeOutFunc("demoImg1");
-		slideFadeInFunc("demoImg2");
-		slideCounter++;
-	}else if (slideCounter === 2){
-		short.domClass("demoImg0")[0].style.display = "none";
-		slideFadeOutFunc("demoImg2");
-		slideFadeInFunc("demoImg3");
-		slideCounter++;
-	}else if (slideCounter === 3){
-		slideFadeOutFunc("demoImg3");
-		slideFadeInFunc("demoImg0");
-		slideCounter 								= 0;
-	};
-};
-	
-//Slider Fade In Image Function
-var slideFadeInFunc 				= function(element){
-	var targetElement           	= short.domClass(element)[0];
-	targetElement.style.display 	= "block";
-	var newSlideFadeValue       	= slideFadeInFrom / 10;
-	targetElement.style.opacity 	= newSlideFadeValue;
-	slideFadeInFrom++;
-		
-	if(slideFadeInFrom == 10){
-		targetElement.style.opacity = 1;
-		clearTimeout(loopTimer3);
-		slideFadeInFrom 			= 0;
-		return false;
-	};
-	var loopTimer3 					= setTimeout('slideFadeInFunc(\''+element+'\')',40);
-};
-	
-//Slider Fade Out Image Function
-var slideFadeOutFunc 				= function(element){
-	var targetElement 				= short.domClass(element)[0];
-	var newSlideFadeValue 			= slideFadeOutFrom / 10;
-	targetElement.style.opacity 	= newSlideFadeValue;
-	slideFadeOutFrom--;
-		
-	if(slideFadeOutFrom == 0){
-		targetElement.style.opacity = 0;
-		targetElement.style.display = "none";
-		clearTimeout(loopTimer4);
-		slideFadeOutFrom 			= 10;
-		return false;
-	};
-	var loopTimer4 					= setTimeout('slideFadeOutFunc(\''+element+'\')',40);
-};
-// End of Slideshow Gallery Player ///////////////////////////////////
-
-
-
-
-
-	
-	
-	
-	
-	
-	
 	
